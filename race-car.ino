3184e64bfa2b1069924d957bff024aa2c320e805
Carl Chan <carl.chan@zuhlke.com>
2019-11-20 13:43:53 +0700

    implemented go back stop

--- race-car.ino
+++ race-car.ino
@@ -1,7 +1,7 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
-#include <WiFiAP.h>
-
+#include <WebServer.h>
+#include <ESPmDNS.h>
 
 #define leftForward 0
 #define leftBackward 1
@@ -19,33 +19,29 @@ const int leftWheelsDirection = 32;
 const int rightWheelsSwitch = 23;
 const int rightWheelsDirection = 22;
 
-void setup() {
-  Serial.begin(115200);
-  WiFi.softAP("Zuhlke Asiaaaa Car", "Not1Less");
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
+WebServer server(80);
 
- 
-//  ledcSetup(leftForward, frequency, resolution);
-//  // left
-//  pinMode(leftWheelsSwitch, OUTPUT);
-//  pinMode(leftWheelsDirection, OUTPUT);
-//  ledcAttachPin(leftWheelsSwitch, leftForward);
-//  ledcAttachPin(leftWheelsDirection, leftBackward);
-//  // right
-//  pinMode(rightWheelsSwitch, OUTPUT);
-//  pinMode(rightWheelsDirection, OUTPUT);
-//  ledcAttachPin(rightWheelsSwitch, rightForward);
-//  ledcAttachPin(rightWheelsDirection, rightBackward);
+void handleHome() {
+  server.send(200, "text/html", "<a href='/go'>Forward</a><br/><a href='/back'>Backward</a><br/><a href='/stop'>Stop</a><br/>");
 }
 
-void brake() {
-  ledcWrite(leftForward, stopSpeedValue);
-  ledcWrite(rightForward, stopSpeedValue);
-  ledcWrite(leftBackward, stopSpeedValue);
-  ledcWrite(rightBackward, stopSpeedValue);
-  delay(1000);
+void handleRoot() {
+  server.send(200, "text/plain", "hello from esp8266!");
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
 }
 
 void moveForward() {
@@ -53,7 +49,7 @@ void moveForward() {
   ledcWrite(rightBackward, stopSpeedValue);
   ledcWrite(leftForward, fullSpeedValue);
   ledcWrite(rightForward, fullSpeedValue);
-  delay(3000);
+  server.send(200, "text/plain", "Go");
 }
 
 void moveBackward() {
@@ -61,11 +57,47 @@ void moveBackward() {
   ledcWrite(rightForward, stopSpeedValue);
   ledcWrite(leftBackward, fullSpeedValue);
   ledcWrite(rightBackward, fullSpeedValue);
-  delay(3000);
+  server.send(200, "text/plain", "Back");
 }
 
-void loop() {
-// moveForward();
-// moveBackward();
-//   brake();
+void brake() {
+  ledcWrite(leftForward, stopSpeedValue);
+  ledcWrite(rightForward, stopSpeedValue);
+  ledcWrite(leftBackward, stopSpeedValue);
+  ledcWrite(rightBackward, stopSpeedValue);
+  server.send(200, "text/plain", "Stop");
+}
+
+void setup(void) {
+  ledcSetup(leftForward, frequency, resolution);
+  // left
+  pinMode(leftWheelsSwitch, OUTPUT);
+  pinMode(leftWheelsDirection, OUTPUT);
+  ledcAttachPin(leftWheelsSwitch, leftForward);
+  ledcAttachPin(leftWheelsDirection, leftBackward);
+  // right
+  pinMode(rightWheelsSwitch, OUTPUT);
+  pinMode(rightWheelsDirection, OUTPUT);
+  ledcAttachPin(rightWheelsSwitch, rightForward);
+  ledcAttachPin(rightWheelsDirection, rightBackward);
+  
+  Serial.begin(115200);
+  WiFi.softAP("Zuhlke Asiaaaa Car", "Not1Less");
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+
+  server.on("/", handleHome);
+  server.on("/go", moveForward);
+  server.on("/back", moveBackward);
+  server.on("/stop", brake);
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+  server.handleClient();
 }
