5ea2ae296c5fd42b6d9703d6b2a204e754eec933
Carl Chan <carl.chan@zuhlke.com>
2019-11-20 16:53:52 +0700

    fixed channels

--- race-car.ino
+++ race-car.ino
@@ -2,11 +2,15 @@
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
+#include <mutex>
+using namespace std;
 
 #define leftForward 0
 #define leftBackward 1
 #define rightForward 2
 #define rightBackward 3
+
+mutex m;
 
 const int frequency = 5000;
 const int resolution = 8;
@@ -45,47 +49,92 @@ void handleNotFound() {
 }
 
 void moveForward() {
+  m.lock();
   ledcWrite(leftBackward, stopSpeedValue);
   ledcWrite(rightBackward, stopSpeedValue);
   ledcWrite(leftForward, fullSpeedValue);
   ledcWrite(rightForward, fullSpeedValue);
+  m.unlock();
   server.send(200, "text/plain", "Go");
 }
 
+void moveForwardSlow() {
+  m.lock();
+  ledcWrite(leftBackward, stopSpeedValue);
+  ledcWrite(rightBackward, stopSpeedValue);
+  ledcWrite(leftForward, slowSpeedValue);
+  ledcWrite(rightForward, slowSpeedValue);
+  m.unlock();
+  server.send(200, "text/plain", "Go Slow");
+}
+
 void moveBackward() {
+  m.lock();
   ledcWrite(leftForward, stopSpeedValue);
   ledcWrite(rightForward, stopSpeedValue);
   ledcWrite(leftBackward, fullSpeedValue);
   ledcWrite(rightBackward, fullSpeedValue);
+  m.unlock();
   server.send(200, "text/plain", "Back");
 }
 
+void moveBackwardSlow() {
+  m.lock();
+  ledcWrite(leftForward, stopSpeedValue);
+  ledcWrite(rightForward, stopSpeedValue);
+  ledcWrite(leftBackward, slowSpeedValue);
+  ledcWrite(rightBackward, slowSpeedValue);
+  m.unlock();
+  server.send(200, "text/plain", "Back Slow");
+}
+
 void moveLeft() {
-  ledcWrite(leftForward, slowSpeedValue);
+  m.lock();
+  ledcWrite(leftForward, stopSpeedValue);
   ledcWrite(rightForward, fullSpeedValue);
   ledcWrite(leftBackward, stopSpeedValue);
   ledcWrite(rightBackward, stopSpeedValue);
   server.send(200, "text/plain", "Left");
+  m.unlock();
 }
 
 void moveRight() {
+  m.lock();
   ledcWrite(leftForward, fullSpeedValue);
-  ledcWrite(rightForward, slowSpeedValue);
+  ledcWrite(rightForward, stopSpeedValue);
   ledcWrite(leftBackward, stopSpeedValue);
   ledcWrite(rightBackward, stopSpeedValue);
+  m.unlock();
   server.send(200, "text/plain", "Right");
+  
+}
+
+void spin() {
+  m.lock();
+  ledcWrite(leftForward, fullSpeedValue);
+  ledcWrite(rightForward, stopSpeedValue);
+  ledcWrite(leftBackward, stopSpeedValue);
+  ledcWrite(rightBackward, fullSpeedValue);
+  m.unlock();
+  server.send(200, "text/plain", "Stop");
 }
 
 void brake() {
+  m.lock();
   ledcWrite(leftForward, stopSpeedValue);
   ledcWrite(rightForward, stopSpeedValue);
   ledcWrite(leftBackward, stopSpeedValue);
   ledcWrite(rightBackward, stopSpeedValue);
+  m.unlock();
   server.send(200, "text/plain", "Stop");
 }
 
 void setup(void) {
   ledcSetup(leftForward, frequency, resolution);
+  ledcSetup(leftBackward, frequency, resolution);
+  ledcSetup(rightForward, frequency, resolution);
+  ledcSetup(rightBackward, frequency, resolution);
+  
   // left
   pinMode(leftWheelsSwitch, OUTPUT);
   pinMode(leftWheelsDirection, OUTPUT);
@@ -105,15 +154,22 @@ void setup(void) {
 
   server.on("/", handleHome);
   server.on("/go", moveForward);
+  server.on("/goSlow", moveForwardSlow);
   server.on("/back", moveBackward);
+  server.on("/backSlow", moveBackwardSlow);
   server.on("/left", moveLeft);
   server.on("/right", moveRight);
+  server.on("/spin", spin);
   server.on("/stop", brake);
 
   server.onNotFound(handleNotFound);
 
   server.begin();
   Serial.println("HTTP server started");
+
+  // ready LED lights up
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop(void) {
